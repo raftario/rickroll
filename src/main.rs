e056bfbaedecf0baf54dc5530a850b8d73583454
Raphaël Thériault <raphael_theriault@outlook.com>
2020-11-19 18:43:52 -0500

    Fix shit I think

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,18 @@ const OPEN: &[u8] = b"open\0";
 const OPEN: &[u8] = b"open\0";
 
 #[cfg(unix)]
+const PATHNAME: &[u8] = b"/bin/sh\0";
+#[cfg(unix)]
+const ARGV: &[*const i8] = &[
+    PATHNAME.as_ptr() as _,
+    b"-c\0".as_ptr() as _,
+    OPEN.as_ptr() as _,
+    URL.as_ptr() as _,
+    core::ptr::null(),
+];
+#[cfg(unix)]
 extern "C" {
-    static environ: *mut *mut i8;
+    static environ: *const *const i8;
 }
 
 #[cfg(unix)]
@@ -21,13 +31,8 @@ extern "C" {
 unsafe extern "C" fn _start() -> ! {
     sc::platform::syscall3(
         sc::platform::nr::EXECVE,
-        b"/bin/sh\0".as_ptr() as _,
-        [
-            b"-c\0".as_ptr() as *const i8,
-            OPEN.as_ptr() as *const i8,
-            URL.as_ptr() as *const i8,
-        ]
-        .as_ptr() as _,
+        PATHNAME.as_ptr() as _,
+        ARGV.as_ptr() as _,
         environ as _,
     );
     sc::platform::syscall1(sc::platform::nr::EXIT, 0);
