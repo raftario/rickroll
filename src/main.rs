513d7376620953c6245e3d1a06fe4facd8ff9c9f
Raphaël Thériault <raphael_theriault@outlook.com>
2020-11-19 17:56:50 -0500

    Get rid of libc on unix and use syscalls directl

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,47 @@
 #![no_std]
 #![no_main]
-#![cfg_attr(target_os = "windows", windows_subsystem = "windows")]
+#![cfg_attr(windows, windows_subsystem = "windows")]
+
+const URL: &[u8] = b"https://www.youtube.com/watch?v=dQw4w9WgXcQ\0";
 
 #[cfg(target_os = "linux")]
+const OPEN: &[u8] = b"xdg-open\0";
+#[cfg(target_os = "macos")]
+const OPEN: &[u8] = b"open\0";
+#[cfg(target_os = "windows")]
+const OPEN: &[u8] = b"open\0";
+
+#[cfg(unix)]
 #[no_mangle]
 unsafe extern "C" fn _start() -> ! {
-    libc::system(b"xdg-open https://www.youtube.com/watch?v=dQw4w9WgXcQ\0".as_ptr() as _);
-    libc::exit(0)
+    syscall::platform::syscall3(
+        syscall::platform::nr::EXECVE,
+        b"/bin/sh\0".as_ptr() as _,
+        [
+            b"-c\0".as_ptr() as *const i8,
+            OPEN.as_ptr() as *const i8,
+            URL.as_ptr() as *const i8,
+        ]
+        .as_ptr() as _,
+        core::ptr::null() as _,
+    );
+    syscall::platform::syscall1(syscall::platform::nr::EXIT, 0);
+    core::hint::unreachable_unchecked()
 }
 
-#[cfg(target_os = "windows")]
+#[cfg(windows)]
 #[no_mangle]
 unsafe extern "C" fn mainCRTStartup() -> ! {
     winapi::um::shellapi::ShellExecuteA(
         core::ptr::null_mut(),
-        b"open\0".as_ptr() as _,
-        b"https://www.youtube.com/watch?v=dQw4w9WgXcQ\0".as_ptr() as _,
+        OPEN.as_ptr() as _,
+        URL.as_ptr() as _,
         core::ptr::null(),
         core::ptr::null(),
         winapi::um::winuser::SW_SHOW,
     );
     winapi::um::processthreadsapi::ExitProcess(0);
     core::hint::unreachable_unchecked()
-}
-
-#[cfg(target_os = "macos")]
-#[no_mangle]
-unsafe extern "C" fn _start() -> ! {
-    libc::system(b"open https://www.youtube.com/watch?v=dQw4w9WgXcQ\0".as_ptr() as _);
-    libc::exit(0)
 }
 
 #[cfg(unix)]
